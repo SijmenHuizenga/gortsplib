090d8b7f6dde9262a28a190a527bbdae6190361c
Sijmen Huizenga <sijmenhuizenga@gmail.com>
2024-09-02 18:35:41 +0200

    Remove duplicate counter

--- client_media.go
+++ client_media.go
@@ -163,14 +163,12 @@ func (cm *clientMedia) writePacketRTCPInQueueUDP(payload []byte) {
 }
 
 func (cm *clientMedia) writePacketRTPInQueueTCP(payload []byte) {
-	atomic.AddUint64(cm.c.BytesSent, uint64(len(payload)))
 	cm.tcpRTPFrame.Payload = payload
 	cm.c.nconn.SetWriteDeadline(time.Now().Add(cm.c.WriteTimeout))
 	cm.c.conn.WriteInterleavedFrame(cm.tcpRTPFrame, cm.tcpBuffer) //nolint:errcheck
 }
 
 func (cm *clientMedia) writePacketRTCPInQueueTCP(payload []byte) {
-	atomic.AddUint64(cm.c.BytesSent, uint64(len(payload)))
 	cm.tcpRTCPFrame.Payload = payload
 	cm.c.nconn.SetWriteDeadline(time.Now().Add(cm.c.WriteTimeout))
 	cm.c.conn.WriteInterleavedFrame(cm.tcpRTCPFrame, cm.tcpBuffer) //nolint:errcheck
